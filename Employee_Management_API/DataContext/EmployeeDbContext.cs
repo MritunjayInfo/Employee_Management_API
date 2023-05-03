a42b6719a57f063fc42aa8271396c71f97900b10
Mritunjay <mritunjaynagvanshi33@gmail.com>
2023-05-04 01:04:06 +0530

    Add project files.

---
+++ Employee_Management_API/DataContext/EmployeeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Employee_Management_API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Employee_Management_API.DataContext
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Employee_Management_API/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Management_API.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? First_Name { get; set; }
+        public string? Last_Name { get; set; }
+        public string? Email_Id { get; set; }
+        public string? Mobile_Number { get; set; }
+        public string? Address { get; set; }
+        public int? Zip_Code { get; set; }
+    }
+}
