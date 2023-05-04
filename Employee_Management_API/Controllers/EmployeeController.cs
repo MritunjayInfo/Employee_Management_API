79fb5aef8bf3a9902b57ac3a387693eb3a006563
Mritunjay <mritunjaynagvanshi33@gmail.com>
2023-05-04 12:47:03 +0530

    GrtMethod

--- Employee_Management_API/Controllers/EmployeeController.cs
+++ Employee_Management_API/Controllers/EmployeeController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Employee_Management_API.DataContext;
+using Employee_Management_API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Employee_Management_API.Controllers
 {
@@ -7,5 +9,19 @@ namespace Employee_Management_API.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private readonly EmployeeDbContext _employeeDbContext;
+
+        public EmployeeController(EmployeeDbContext employeeDbContext)
+        {
+            _employeeDbContext = employeeDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAll()
+        {
+            var employees = await _employeeDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
     }
 }
