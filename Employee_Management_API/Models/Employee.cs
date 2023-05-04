647cffebed760a7692985e37cd5c29af2fa07a35
Mritunjay <mritunjaynagvanshi33@gmail.com>
2023-05-04 13:27:00 +0530

    Third commit

--- Employee_Management_API/Models/Employee.cs
+++ Employee_Management_API/Models/Employee.cs
@@ -6,10 +6,13 @@ namespace Employee_Management_API.Models
     {
         [Key]
         public int Id { get; set; }
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Use letters only please")]
         public string? First_Name { get; set; }
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Use letters only please")]
         public string? Last_Name { get; set; }
         public string? Email_Id { get; set; }
         public string? Mobile_Number { get; set; }
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Use letters only please")]
         public string? Address { get; set; }
         public int? Zip_Code { get; set; }
     }
